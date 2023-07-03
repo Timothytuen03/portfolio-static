8d58592c4121e03fd89a28d30fc2383bd0a0aa03
Timothy Tuen <timothytuen45@gmail.com>
2023-07-03 16:46:09 -0400

    Mobile responsiveness added to navbars and to education page

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ navbar.addEventListener("click", function() {
     n.classList.toggle("hidden");
     console.log("clicked");
 })
+
+const relCour = document.getElementsByClassName("relCourse");
+// relCour.array.forEach(element => {
+//     element.addEventListener("click", function() {
+//         const idName = element.id + "_class";
+//         const content = document.getElementById(idName);
+//         content.classList.toggle("hidden");
+//     })
+// });
+
+for(let i = 0; i < relCour.length; i++) {
+    relCour[i].addEventListener("click", function(event) {
+        // console.log(event.target.id);
+        const idName = event.target.id + "_classMobile";
+        console.log(idName);
+        console.log(document.getElementById(idName));
+        document.getElementById(idName).classList.toggle("hidden");
+    })
+}
